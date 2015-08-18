0969ce6cf815bb9dd56fd17277f07b96df29cd73
Simon Westphahl <westphahl@gmail.com>
2015-08-18 19:31:11 +0200

    doc: create Sphinx documentation skeleton

--- boilerplate/script.py
+++ boilerplate/script.py
@@ -3,6 +3,7 @@ import logging
 
 
 def main():
+    """Boilerplate example command line interface"""
     parser = argparse.ArgumentParser(description="Boilerplate example script")
     parser.add_argument('--level', '-l', default='info',
                         choices=['debug', 'info', 'warning', 'error', 'critical', ],
