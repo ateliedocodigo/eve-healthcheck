22bd3052a3e8f5cff4089507facab94d189a0709
Luis Fernando Gomes <dev@luiscoms.com.br>
2017-02-21 02:08:05 -0300

    Add EveHealthCheck

--- eve_healthcheck/__init__.py
+++ eve_healthcheck/__init__.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from healthcheck import HealthCheck
+
+import logging
+
+
+class EveHealthCheck(object):
+
+    def __init__(self, app=None, healthcheck_uri='/healthcheck', *argv, **kw):
+        self.logger = logging.getLogger(self.__module__)
+        self.hc = HealthCheck(*argv, **kw)
+
+        if app:
+            self.init_app(app, healthcheck_uri)
+
+    def init_app(self, app, healthcheck_uri='/healthcheck'):
+
+        def database_check():
+            # perform a count
+            for k, v in app.config.get('DOMAIN', {}).items():
+                result = app.data.is_empty(k)
+                assert type(result) == bool, "Database NOK"
+            return True, "Database OK"
+
+        self.hc.add_check(database_check)
+        app.add_url_rule(healthcheck_uri, healthcheck_uri, view_func=lambda: self.hc.run())
+        for k, v in app.config.get('DOMAIN', {}).items():
+            self.logger.debug("Adding uri /{}{}".format(k, healthcheck_uri))
+            app.add_url_rule("/{}{}".format(k, healthcheck_uri),
+                             "/{}{}".format(k, healthcheck_uri),
+                             view_func=lambda: self.hc.run())
--- tests/test_healthckeck.py
+++ tests/test_healthckeck.py
@@ -1,13 +1,51 @@
-import pytest
-from nose.tools import raises
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+from tests import TestBase
 
 
-class TestExample(object):
+class TestEveHealthchek(TestBase):
 
-    def test_example(self):
-        assert True, "This should not fail"
+    def test_api(self):
+        self.app.debug = True
+        r = self.test_client.get('/')
+        self.assertEqual(r.status_code, 200)
 
-    @pytest.mark.xfail
-    @raises(AssertionError)
-    def test_failing(self):
-        assert False, "This will always fail"
+    def test_basic_check(self):
+
+        self.app.debug = True
+
+        r = self.test_client.get(self.healthcheck_uri)
+        self.assertEqual(200, r.status_code)
+
+    def test_failing_check(self):
+        def fail_check():
+            return False, "FAIL"
+
+        self.hc.hc.add_check(fail_check)
+        r = self.test_client.get(self.healthcheck_uri)
+        self.assertEqual(500, r.status_code)
+
+        jr = self.parse_response(r)
+        self.assertEqual("failure", jr["status"])
+
+    def test_database_success_check(self):
+        # ckeck eatch domain
+        for k, v in self.app.config.get('DOMAIN', {}).items():
+            r = self.test_client.get("/{}{}".format(k, self.healthcheck_uri))
+            self.assertEqual(200, r.status_code)
+
+            jr = self.parse_response(r)
+            self.assertEqual("success", jr["status"])
+
+    def test_database_failure_check(self):
+        # set an invalid database
+        self.app.config['MONGO_URI'] = 'mongodb://invalid.db.net/db'
+        # ckeck eatch domain
+        for k, v in self.app.config.get('DOMAIN', {}).items():
+            r = self.test_client.get("/{}{}".format(k, self.healthcheck_uri))
+
+            self.assertEqual(500, r.status_code)
+            jr = self.parse_response(r)
+            self.assertEqual("failure", jr["status"])
