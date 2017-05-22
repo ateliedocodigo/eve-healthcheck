c068f0b6c10416b977af27926f7fc7b4a1bcaef3
Luis Fernando Gomes <dev@luiscoms.com.br>
2017-05-22 11:24:28 -0300

    Fix #1 - Support for URL_PREFIX and API_VERSION config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 eve-healthckeck
 ===========
 
+Next Release
+-----
+
+* Fix #1 - Support for URL_PREFIX and API_VERSION config
+
 0.1.0
 -----
 
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,8 +3,8 @@
 
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
-MONGO_USERNAME = 'test_user'
-MONGO_PASSWORD = 'test_pw'
+MONGO_USERNAME = ''  # 'test_user'
+MONGO_PASSWORD = ''  # 'test_pw'
 MONGO_DBNAME = 'eve_healthcheck_test'
 
 TRANSPARENT_SCHEMA_RULES = True
